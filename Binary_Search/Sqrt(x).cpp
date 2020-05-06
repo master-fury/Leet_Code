4553cc60c024a61f615dc272dda5124ec69acdcd
Manish Kumar <master20121997@gmail.com>
2020-05-06 23:49:27 +0530

    Sqrt(x).cpp

---
+++ Binary_Search/Sqrt(x).cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low=1;
+        int high=x;
+        if(x<2)
+            return x;
+        while(low<=high){
+            int mid=low+ (high-low)/2;
+            if(mid==x/mid) return mid;
+            else if ((mid)>x/mid)
+                high=mid-1;
+            else
+                low=mid+1;
+        }
+        return high;
+    }
+};
